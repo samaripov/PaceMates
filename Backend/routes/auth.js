6986457a950e1bfd7fd7bd3a5d403b6945b82ce3
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2025-12-26 18:50:44 -0500

    Added signup functionality.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,32 +6,67 @@ const crypto = require("crypto");
 const db = require("../db");
 
 //Routes
-router.get("/login", function(req, res, next) {
+// - Login
+router.get("/login", function (req, res, next) {
     res.render("login");
-})
+});
 
 router.post("/login/password", passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login"
-}))
+}));
 
-router.post("/logout", function(req, res, next) {
-    req.logout(function(err) {
+// - Logout
+router.post("/logout", function (req, res, next) {
+    req.logout(function (err) {
         if (err) { return next(err); }
         res.redirect("/");
-    })
-})
+    });
+});
 
+// - Signup
+router.get("/signup", function (req, res, next) {
+    res.render("signup");
+});
+
+router.post("/signup", function (req, res, next) {
+    const salt = crypto.randomBytes(16);
+    if (req.body.password !== req.body.confirm_password) {
+        return res.redirect("/signup");
+    }
+    crypto.pbkdf2(req.body.password, salt, 310000, 32, "sha256", function (err, hashedPassword) {
+        if (err) { return next(err); }
+        db.run(
+            "INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)", 
+            [
+                req.body.username,
+                hashedPassword,
+                salt
+            ],
+            function(err) {
+                if (err) { return next(err); }
+                const user = {
+                    id: this.lastID,
+                    username: req.body.username
+                };
+                req.login(user, function(err) {
+                    if (err) { return next(err); }
+                    res.redirect("/")
+                })
+            }
+        )
+    });
+})
 //Auth Strategy
 passport.use(new LocalStrategy(function verify(username, password, done) {
-    db.get("SELECT * FROM users WHERE username = ?", [username], function(err, row) {
-        if(err) { done(err); }
-        if(!row) { return done(null, false, { message: "Incorrect username or password"}); }
+    db.get("SELECT * FROM users WHERE username = ?", [username], function (err, row) {
+        if (err) { done(err); }
+        if (!row) { return done(null, false, { message: "Incorrect username or password" }); }
 
-        crypto.pbkdf2(password, row.salt, 310000, 32, "sha256", function(err, hashedPassword){
+        crypto.pbkdf2(password, row.salt, 310000, 32, "sha256", function (err, hashedPassword) {
             if (err) { return done(err); }
-            if(!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-                return done(null, false, { message: "Incorrect username or password"});
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return done(null, false, { message: "Incorrect username or password" });
             }
             return done(null, row);
         });
